33ca6ee3c2ba58dd61f18103662bcb67b2af7521
sofihoyos <169062995+sofihoyos@users.noreply.github.com>
2024-10-23 08:40:30 -0500

    button

--- index.js
+++ index.js
@@ -5,12 +5,26 @@ const pause = document.getElementById ('pause')
 const forward = document.getElementById ('forward')
 const rewind = document.getElementById ('rewind')
 const stop = document.getElementById ('stop')
+const previous = document.getElementById ('previous')
+const next = document.getElementById('next')
 
-play.addEventListener('click', () => audio.play())
-pause.addEventListener('click', () => audio.pause())
-rewind.addEventListener('click', () => audio.currentTime -=10)
-forward.addEventListener('click', () => audio.currentTime +=10)
-stop.addEventListener('click', () =>{
-    audio.pause()
-    audio.currentTime = 0
-})
+play.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        play.classList.remove('bx-play');
+        play.classList.add('bx-pause');
+    } else {
+        audio.pause();
+        play.classList.remove('bx-pause');
+        play.classList.add('bx-play');
+    }
+});
+
+rewind.addEventListener('click', () => audio.currentTime -= 10);
+forward.addEventListener('click', () => audio.currentTime += 10);
+stop.addEventListener('click', () => {
+    audio.pause();
+    audio.currentTime = 0;
+    play.classList.remove('bx-pause');
+    play.classList.add('bx-play');
+});
