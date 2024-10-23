96af709d92c90d53566a3f87b2119c6912e7f3f4
sofihoyos <169062995+sofihoyos@users.noreply.github.com>
2024-10-23 09:14:34 -0500

    button

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const stop = document.getElementById('stop');
 const previous = document.getElementById('previous');
 const next = document.getElementById('next');
 
+let currentSongIndex = 0;
 
+function loadSong(songIndex) {
+    const song = playList[songIndex];
+    audio.src = song.song; 
+    document.getElementById('player__song').textContent = song.title; 
+    document.getElementById('player__artist').textContent = song.artist; 
+    document.getElementById('player__img').src = song.img;
+}
 
 play.addEventListener('click', () => {
     if (audio.paused) {
@@ -31,3 +39,22 @@ stop.addEventListener('click', () => {
     play.classList.add('bx-play');
 });
 
+
+previous.addEventListener('click', () => {
+    currentSongIndex = (currentSongIndex > 0) ? currentSongIndex - 1 : playList.length - 1;
+    loadSong(currentSongIndex);
+    audio.play();
+    play.classList.remove('bx-play');
+    play.classList.add('bx-pause');
+});
+
+
+next.addEventListener('click', () => {
+    currentSongIndex = (currentSongIndex < playList.length - 1) ? currentSongIndex + 1 : 0;
+    loadSong(currentSongIndex);
+    audio.play();
+    play.classList.remove('bx-play');
+    play.classList.add('bx-pause');
+});
+
+loadSong(currentSongIndex);
